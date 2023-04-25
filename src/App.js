581080c4bf1bf9b5973283ca52badf4f5154670a
divyachandar <dvrmahanty@gmail.com>
2023-04-25 11:32:42 +0000

    success

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import {Component} from 'react'
+import Clock from './components/Clock'
+
+import './App.css'
+
+class App extends Component {
+  state = {showClock: false}
+
+  onToggleClick = () => {
+    this.setState(prevState => {
+      const {showClock} = prevState
+      return {showClock: !showClock}
+    })
+  }
+
+  render() {
+    const {showClock} = this.state
+    return (
+      <div className="app-container">
+        <button
+          type="button"
+          onClick={this.onToggleClick}
+          className="toggle-button"
+        >
+          {showClock ? 'Hide Clock' : 'Show Clock'}
+        </button>
+        {showClock && <Clock />}
+      </div>
+    )
+  }
+}
+
+export default App
